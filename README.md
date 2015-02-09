52d540a245da0c6cc9edc81a457b724c7de60bf5
agmenc <chris.agmen-smith@email.com>
2015-02-09 21:51:56 +0000

    Add some code examples to README and show a better worked example in PlusSpec

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
+Planet7 Extensions
+==================
 
-Currently just a place for me to play with Shapeless to see if it can solve some problems in the world of CSV mungeing
+Based on the excellent examples provided with Shapeless, planet7-extensions adds a couple of handy features to Planet7.
 
-FYI, Shapeless rocks. I don't understand how it works. I don't understand the types of problems that it solves, except that it solves one problem that is dear to my heart: shipping data between CSVs and case classes
+https://github.com/milessabin/shapeless/blob/master/examples/src/main/scala/shapeless/examples/
+
+CSV Row <=> Case Class
+----------------------
+
+```scala
+"ConvertTo case class from Row" in {
+  val row: Row = Row(Array("5", "Jeremiah", "Jones", "13.3"))
+
+  val triedPerson = ConvertTo[ActualPerson].fromRow(row).get
+
+  triedPerson mustEqual ActualPerson(5, "Jeremiah", "Jones", 13.3)
+}
+
+"ConvertTo to Row from case class" in {
+  val person = ActualPerson(5, "Jeremiah", "Jones", 13.3)
+
+  val row = ConvertFrom[ActualPerson].toRow(person)
+
+  row mustEqual Row(Array("5", "Jeremiah", "Jones", "13.3"))
+}
+```
+
+Case Class + Case Class
+-----------------------
+
+```scala
+"Adding case classes sums Numerics, ORs Booleans and prefers the first nonEmpty String" in {
+  val buy = Transaction("1", "buy", -26.3, 20)
+  val sell = Transaction("1", "sell", 72.4, -30)
+
+  buy |+| sell mustEqual Transaction("1", "buy", 46.1, -10)
+}
+```
--- src/test/scala/planet7/extensions/implementation/PlusSpec.scala
+++ src/test/scala/planet7/extensions/implementation/PlusSpec.scala
@@ -3,20 +3,37 @@ package planet7.extensions.implementation
 import org.scalatest.{MustMatchers, WordSpec}
 import planet7.extensions._
 
-case class Transaction(account: Int, desc: String, amount: BigDecimal)
+case class Transaction(accountId: String, desc: String, price: BigDecimal, lots: Int)
 
 class PlusSpec extends WordSpec with MustMatchers {
-  "I can add case classes" in {
+  "Adding case classes sums Numerics, ORs Booleans and prefers the first nonEmpty String" in {
+    val buy = Transaction("1", "buy", -26.3, 20)
+    val sell = Transaction("1", "sell", 72.4, -30)
+
+    buy |+| sell mustEqual Transaction("1", "buy", 46.1, -10)
+  }
+
+  "Worked example" in {
     val transactions: Seq[Transaction] = Seq(
-      Transaction(1, "buy X", -26.3),
-      Transaction(1, "sell Y", 72.4),
-      Transaction(2, "sell Z", 15.2),
-      Transaction(3, "buy A", -29.3),
-      Transaction(2, "sell B", 73.8),
-      Transaction(1, "buy C", -93.2))
+      Transaction("1", "buy", -26.3,  10),
+      Transaction("1", "sell", 72.4, -10),
+      Transaction("2", "sell", 15.2, -10),
+      Transaction("3", "buy", -29.3,  10),
+      Transaction("2", "sell", 73.8, -10),
+      Transaction("1", "buy", -93.2,  10))
+    
+    val expectedBalances = Seq(
+      Transaction("1", "balance", -47.1,  10),
+      Transaction("2", "balance",  89.0, -20),
+      Transaction("3", "balance", -29.3,  10))
 
-    val result = transactions.reduceLeft((t1, t2) => t1 |+| t2)
+    val result = transactions
+      .groupBy(_.accountId)
+      .mapValues(st => st.reduceLeft((t1, t2) => t1 |+| t2))
+      .values.toSeq
+      .map(_.copy(desc = "balance"))
+      .sortBy(_.accountId)
 
-    result mustEqual Transaction(10,"buy X",12.6)
+    result mustEqual expectedBalances
   }
 }
