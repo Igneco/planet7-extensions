e3e6c48c9c5e480b0656b7ec739425c0e42c3c3e
agmenc <chris.agmen-smith@email.com>
2015-07-17 16:06:34 +0100

    Version 0.0.7 - upgrade scala version and reduce transitive dependency noise.

--- project/Build.scala
+++ project/Build.scala
@@ -4,27 +4,19 @@ import Keys._
 object Build extends Build {
 
   def sharedSettings = Seq(
-    scalaVersion:= "2.11.5",
+    scalaVersion:= "2.11.7",
     scalacOptions += "-deprecation",
     resolvers ++= Seq(
       Resolver.sonatypeRepo("releases"),
       Resolver.sonatypeRepo("snapshots")
     ),
-    crossScalaVersions := Seq("2.11.5"),
+    crossScalaVersions := Seq("2.11.7"),
     libraryDependencies ++= Seq(
       "com.chuusai" %% "shapeless" % "2.1.0-RC2",
-      "com.github.agmenc" %% "planet7" % "0.1.10",
+      "com.github.agmenc" %% "planet7" % "0.1.14",
       "junit" % "junit" % "4.11" % "test",
       "org.scalatest" %% "scalatest" % "2.2.0" % "test"
-    ),
-    // add scala-xml dependency when needed (for Scala 2.11 and newer)
-    // this mechanism supports cross-version publishing
-    libraryDependencies := {
-      CrossVersion.partialVersion(scalaVersion.value) match {
-        case Some((2, scalaMajor)) if scalaMajor >= 11 => libraryDependencies.value :+ "org.scala-lang.modules" %% "scala-xml" % "1.0.3"
-        case _ => libraryDependencies.value
-      }
-    }
+    )
   )
 
   lazy val main = Project(id = "planet7-extensions", base = file(".")).settings(sharedSettings: _*)
